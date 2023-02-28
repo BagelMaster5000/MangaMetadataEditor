ae580d067304e8610df95722e595cef5cc0b130e
Sammy Mahmoudi <sammymahmoudi@gmail.com>
2023-02-27 22:47:01 -0600

    Added functionality to custom fields. Added processing progress message

--- PDFMetaDataUpdater/MainWindow.xaml.cs
+++ PDFMetaDataUpdater/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using PdfSharp.Pdf.IO;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using Path = System.IO.Path;
 
@@ -11,18 +13,36 @@ namespace PDFMetaDataUpdater {
 	public partial class MainWindow : Window {
 		List<string> selectedFilePaths;
 
+		string curMangaTitle = "MangaTitle";
+		string curMangaAuthor = "MangaAuthor";
+		int curPadding0s = 2;
+
 		#region Setup
 		public MainWindow() {
 			InitializeComponent();
 			OpenPdfFileEditingStream();
+			InitializeInputFields();
 
 			RefreshFilesSelectedMessage();
+			RefreshNewFormattingPreview();
+			ClearRunningProgressMessage();
+			EnableRunButton();
+
 		}
+
+		private void InitializeInputFields() {
+			TitleField.Text = curMangaTitle;
+			AuthorField.Text = curMangaAuthor;
+			Padding0sSlider.Value = curPadding0s;
+		}
+
 		private static void OpenPdfFileEditingStream() {
 			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 		}
 		#endregion
 
+
+		#region Open Files Dialog
 		OpenFileDialog openFileDialog = new OpenFileDialog();
 		private void SelectPdfFiles(object sender, RoutedEventArgs e) {
 			openFileDialog.Multiselect = true;
@@ -32,7 +52,26 @@ namespace PDFMetaDataUpdater {
 				RefreshFilesSelectedMessage();
 			}
 		}
+		#endregion
 
+
+		#region Input Fields
+		private void SaveEnteredTitle(object sender, RoutedEventArgs e) {
+			curMangaTitle = TitleField.Text;
+			RefreshNewFormattingPreview();
+		}
+		private void SaveEnteredAuthor(object sender, RoutedEventArgs e) {
+			curMangaAuthor = AuthorField.Text;
+			RefreshNewFormattingPreview();
+		}
+		private void SaveEnteredPadding0s(object sender, RoutedEventArgs e) {
+			curPadding0s = (int)Padding0sSlider.Value;
+			RefreshNewFormattingPreview();
+		}
+		#endregion
+
+
+		#region Running
 		private void RunFileProcessing(object sender, RoutedEventArgs e) {
 			if (selectedFilePaths == null) {
 				return;
@@ -48,30 +87,81 @@ namespace PDFMetaDataUpdater {
 
 			selectedFilePaths.Sort((s1, s2) => string.Compare(s1, s2));
 
+			CreateUpdatedFilesInNewDirectory(newDirectoryName);
+
 			// Create updated pdf files in new directory
-			int counter = 1;
-			foreach (string filePath in selectedFilePaths) {
-				PdfDocument document = PdfReader.Open(filePath);
-				//string oldName = Path.GetFileName(filePath);
-				//string oldTitle = document.Info.Title;
+			//int counter = 1;
+			//foreach (string filePath in selectedFilePaths) {
+			//	RefreshingRunningProgressMessage(counter, selectedFilePaths.Count);
 
-				string volumeNumber = counter.ToString().PadLeft(3, '0');
-				string newName = "One Piece v" + volumeNumber + ".pdf";
-				string newTitle = "One Piece v" + volumeNumber;
-				counter++;
+			//	string volumeNumber = counter.ToString().PadLeft(3, '0');
+			//	string newName = "One Piece v" + volumeNumber + ".pdf";
+			//	string newTitle = "One Piece v" + volumeNumber;
 
-				document.Info.Title = newTitle;
-				document.Info.Author = "Oda";
-				document.Save(newDirectoryName + "/" + newName);
-			}
+			//	PdfDocument document = PdfReader.Open(filePath);
+			//	document.Info.Title = newTitle;
+			//	document.Info.Author = "Oda";
+			//	document.Save(newDirectoryName + "/" + newName);
+
+			//	counter++;
+			//}
+
+			//ClearRunningProgressMessage();
 		}
 
+		private async void CreateUpdatedFilesInNewDirectory(string newDirectory) {
+			DisableRunButton();
+
+			int counter = 1;
+			foreach (string filePath in selectedFilePaths) {
+				RefreshingRunningProgressMessage(counter, selectedFilePaths.Count);
+
+				string volumeNumber = counter.ToString().PadLeft(curPadding0s, '0');
+				string newName = curMangaTitle + " v" + volumeNumber + ".pdf";
+				string newTitle = curMangaTitle + " v" + volumeNumber;
+
+				PdfDocument document = PdfReader.Open(filePath);
+				document.Info.Title = newTitle;
+				document.Info.Author = curMangaAuthor;
+				document.Save(newDirectory + "/" + newName);
+
+				counter++;
+				await Task.Delay(5);
+			}
+
+			ClearRunningProgressMessage();
+
+			EnableRunButton();
+		}
+
+		#endregion
 
 
+		private void EnableRunButton() => RunButton.IsEnabled = true;
+		private void DisableRunButton() => RunButton.IsEnabled = false;
 
+		#region Refreshing frontend
 		private void RefreshFilesSelectedMessage() {
 			int numFilesSelected = (selectedFilePaths == null) ? 0 : selectedFilePaths.Count;
 			FilesSelectedMessage.Text = numFilesSelected + " files selected";
 		}
+
+		private void RefreshNewFormattingPreview() {
+			string padding0sPreview = "";
+			for (int p = 0; p < curPadding0s; p++) {
+				padding0sPreview += '#';
+			}
+			NewFormattingPreviewTitle.Text = "Title: " + curMangaTitle + " v" + padding0sPreview;
+
+			NewFormattingPreviewAuthor.Text = "Author: " + curMangaAuthor;
+		}
+
+		private void RefreshingRunningProgressMessage(int curDocument, int totalDocuments) {
+			RunningProgressMessage.Text = "Processing " + curDocument + " / " + totalDocuments + "...";
+		}
+		private void ClearRunningProgressMessage() {
+			RunningProgressMessage.Text = "";
+		}
+		#endregion
 	}
 }
